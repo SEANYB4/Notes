86b1901a3287577389f281fef672a8c2baba38fa
Sean Bain <75031999+SEANYB4@users.noreply.github.com>
2021-11-20 17:01:45 +0000

    Apple watch note taking app.

---
+++ Notes WatchKit Extension/Model/Note.swift
@@ -0,0 +1,17 @@
+//
+//  Note.swift
+//  Notes WatchKit Extension
+//
+//  Created by Sean Bain on 20/11/2021.
+//
+
+import Foundation
+
+
+struct Note: Identifiable, Codable {
+    
+    let id: UUID
+    let text: String
+    
+    
+}
